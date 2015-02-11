1114327fb3683777475626f14ffce20802728373
mvaneijgen <mvaneijgen@gmail.com>
2015-02-11 16:36:01 +0100

    Localhost working
    127.0.0.1:4000 on chrome for some reason

---
+++ _site/gruntfile.js
@@ -0,0 +1,63 @@
+/* globals module, require */
+
+module.exports = function(grunt) {
+
+  "use strict";
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+
+    sass: {
+      global: {
+        options: {
+          style: "compressed"
+        },
+        files: {
+          "css/main-unprefixed.css": "_sass/main.scss"
+        }
+      }
+    },
+
+    autoprefixer: {
+      global: {
+        src: "css/main-unprefixed.css",
+        dest: "css/main.css"
+      }
+    },
+
+    shell: {
+      jekyllServe: {
+        command: "jekyll serve --baseurl="
+      },
+      jekyllBuild: {
+        command: "jekyll build --config _config-dev.yml"
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      site: {
+        files: ["index.html", "writing.html", "about.html", "_layouts/*.html", "_posts/*.md", "_projects/*.md", "_includes/*.html"],
+        tasks: ["shell:jekyllBuild"]
+      },
+      js: {
+        files: ["js/*.js"],
+        tasks: ["uglify", "shell:jekyllBuild"]
+      },
+      css: {
+        files: ["scss/*.scss"],
+        tasks: ["sass", "autoprefixer", "shell:jekyllBuild"]
+      }
+
+    }
+
+  });
+
+  require("load-grunt-tasks")(grunt);
+
+  grunt.registerTask("serve", ["shell:jekyllServe"]);
+  grunt.registerTask("default", ["sass", "autoprefixer", "shell:jekyllBuild", "watch"]);
+
+};
